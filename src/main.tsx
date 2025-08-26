29636c04063730c79501d071277f8c2008671ba2
Brett Piggott <bapiggott@oakland.edu>
2025-08-26 13:57:16 -0400

    Update site

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { HashRouter } from "react-router-dom";
 import App from "./App";
 import "./index.css";
+
+// ✅ HashRouter works everywhere on GitHub Pages
+import { HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
